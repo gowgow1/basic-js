1d6fdeffd0473da46a5e6c403239c461978b6e30
gowgow1 <gow_gow1@mail.ru>
2021-03-04 01:21:45 +0300

    8 / 10

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (Array.isArray(arr)){
+      if (arr.length==0){return 1}
+      return 1 + Math.max(...arr.map(i=> this.calculateDepth(i)))
+
+      }else{return 0}
+    
   }
-};
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)){ 
+    let newA = arr.slice();
+
+      for (let i=0;i < arr.length; i++){
+        
+        if (arr[i] == "--discard-next"){
+            newA.splice(i,1,"fo")
+            if (i < arr.length-1){
+              newA.splice(i+1,1,"fo")
+          }
+
+        } else if (arr[i] == "--discard-prev"){
+            
+          newA.splice(i,1,"fo");
+            if (i > 0){
+              newA.splice(i-1,1,"fo")
+            }
+
+        } else if (arr[i] == "--double-next" ){
+            
+          newA.splice(i,1,"fo")
+            if(i<arr.length-1){
+              newA.splice(i,1,newA[i+1])
+            }
+
+        } else if (arr[i] == "--double-prev" ){
+            newA.splice(i,1,"fo")
+            if(i > 0){
+              newA.splice(i,1,newA[i-1])
+          }
+        }
+      } return newA.filter(a => (a!="fo")? true : false)
+
+    }throw new Error();
 };
+
