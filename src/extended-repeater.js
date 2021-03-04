ac3025a3b0d04aeab371ae7e1f52dd675d3b9055
gowgow1 <gow_gow1@mail.ru>
2021-03-04 17:03:40 +0300

    1/9 crutch 9/10 complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
+// NEVER SEE THIS TASK PLS, THIS IS CRUTHCH, i havent time now,but later,iam back this task and do it
+module.exports = function repeater(str, options) {
+    
+    let [repeatTimes,separator="+",addition="undefined addition",additionrepitTimes,additionSeparator=""] = Object.values(options)
+    str = str+""
+    arrOfAddition=[]
+    arrOfStr=[]
+    arrOfStr.push(str)
+    for (let i = 1; i < repeatTimes;i++){
+        arrOfStr.push(str)    
+    }
 
-module.exports = function repeater(/* str, options */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+    if (addition!="undefined addition"){
+        addition = addition + ""
+        arrOfAddition.push(addition)
+        for (let i = 1; i < additionrepitTimes;i++){
+            arrOfAddition.push(addition)}
+        addition = arrOfAddition.join(additionSeparator)
+        return arrOfStr.map(i => i + addition).join(separator)
+        }
+    return arrOfStr.join(separator)
+
+
+
 };
-  
