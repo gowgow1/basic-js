bc22c946c88dac6b68ad812d51271918f841968d
gowgow1 <gow_gow1@mail.ru>
2021-03-05 00:36:20 +0300

    dasa

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,27 +1,26 @@
 const CustomError = require("../extensions/custom-error");
-// NEVER SEE THIS TASK PLS, THIS IS CRUTHCH, i havent time now,but later,iam back this task and do it
+// NEVER SEE THIS TASK PLS, THIS IS CRUTHCH, i havent time now,but later,iam back this task and do
 module.exports = function repeater(str, options) {
     
-    let [repeatTimes,separator="+",addition="undefined addition",additionrepitTimes,additionSeparator=""] = Object.values(options)
-    str = str+""
-    arrOfAddition=[]
-    arrOfStr=[]
+    let {repeatTimes=0,separator="+",addition="undefined addition",additionRepeatTimes=0,additionSeparator="|"} = options
+  str = str+""
+  console.log(addition)
+  arrOfAddition=[]
+  arrOfStr=[]
+  arrOfStr.push(str)
+  for (let i = 1; i < repeatTimes;i++){
     arrOfStr.push(str)
-    for (let i = 1; i < repeatTimes;i++){
-        arrOfStr.push(str)    
-    }
+  }
 
-
-    if (addition!="undefined addition"){
-        addition = addition + ""
-        arrOfAddition.push(addition)
-        for (let i = 1; i < additionrepitTimes;i++){
-            arrOfAddition.push(addition)}
-        addition = arrOfAddition.join(additionSeparator)
-        return arrOfStr.map(i => i + addition).join(separator)
-        }
-    return arrOfStr.join(separator)
-
-
-
+  if (addition!="undefined addition"){
+    addition = addition + ""
+    arrOfAddition.push(addition)
+      for (let i = 1; i < additionRepeatTimes;i++){
+        arrOfAddition.push(addition)}
+    addition = arrOfAddition.join(additionSeparator)
+    return arrOfStr.map(i => i + addition).join(separator)
+  }
+  return arrOfStr.join(separator)
 };
+
+
